3bfc34c833b4c62a118ba821a94143aceeab6410
482F <482F.git.mail@482f.net>
2023-07-23 00:00:00 +0900

    対象のオブジェクトの型を検査する関数を追加

--- src/json.ts
+++ src/json.ts
@@ -1,4 +1,4 @@
-import { isNotNullish } from './common.ts'
+import { type Result, isNotNullish } from './common.ts'
 
 export type JsonKey = number | string
 export type JsonPrimitive = boolean | number | string | null
@@ -31,4 +31,92 @@ function _isJson(
 }
 export function isJson(val: unknown): val is Json {
   return _isJson(val, new Set())
+}
+
+function _doExtends<T, M>(
+  target: T,
+  model: M,
+  {
+    keys,
+  }: Readonly<{
+    keys: readonly string[]
+  }>
+): Result<M & T> {
+  function getResult(isSucceeded: true): [M & T, undefined]
+  function getResult(
+    isSucceeded: false,
+    error: Readonly<Error> | null
+  ): [undefined, Readonly<Error>]
+  function getResult(
+    isSucceeded: boolean,
+    error?: Readonly<Error> | null
+  ): Result<M & T> {
+    if (isSucceeded) {
+      return [target as M & T, undefined]
+    }
+    const joinedKeys = keys.map((key) => `['${key}']`).join('')
+    return [
+      undefined,
+      error ??
+        new Error(`target と model が一致しませんでした。keys: ${joinedKeys}`),
+    ]
+  }
+
+  if (typeof target !== typeof model) {
+    return getResult(false, null)
+  }
+
+  // @ts-expect-error target と model が同一の場合は無条件で extends とみなす
+  if (target === model) {
+    return [target, undefined]
+  }
+
+  if (isJsonPrimitive(target)) {
+    if (!isJsonPrimitive(model)) {
+      return getResult(false, null)
+    }
+    if (typeof target === typeof model) {
+      return getResult(true)
+    }
+  }
+
+  if (Array.isArray(target)) {
+    if (!Array.isArray(model)) {
+      return getResult(false, null)
+    }
+    const targetArray: unknown[] = target
+
+    const [modelItem]: unknown[] = model
+    for (const [i, targetItem] of Object.entries(targetArray)) {
+      const [, error] = _doExtends(targetItem, modelItem, {
+        keys: [...keys, i],
+      })
+      if (error) {
+        return getResult(false, error)
+      }
+    }
+    return getResult(true)
+  }
+
+  if (isNotNullish(target)) {
+    if (!isNotNullish(model)) {
+      return getResult(false, null)
+    }
+
+    for (const key of Object.keys(model)) {
+      const [, error] = _doExtends(target[key], model[key], {
+        keys: [...keys, key],
+      })
+      if (error) {
+        return getResult(false, error)
+      }
+    }
+    return getResult(true)
+  }
+
+  return getResult(false, null)
+}
+
+export function doExtends<T, M>(target: T, model: M): Result<M & T> {
+  return _doExtends(target, model, { keys: [] })
 }
